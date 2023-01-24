b81a30c9fe037b3caa4300880be77b01bd1490ba
Danigy <dnlmdwrk@gmail.com>
2023-01-24 12:53:32 -0800

    printf team 'Daniel & Weldsh' project update

--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -20,9 +19,9 @@
 #define S_SHORT 1
 
 /**
- * struct fmt - Struct oprations
- * @fmt: The format
- * @fn: The function associated
+ * struct fmt - Struct op
+ * @fmt: The format.
+ * @fn: The function associated.
  */
 
 struct fmt
@@ -33,6 +32,7 @@ struct fmt
 
 /**
  * typedef struct fmt fmt_t - Struct op
+ *
  * @fmt: The format.
  * @fm_t: The function associated.
  */
@@ -43,7 +43,7 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 		 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/******** FUNCTIONS ********/
+/* FUNCTIONS */
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
@@ -66,10 +66,11 @@ int print_hexadecimal(va_list types, char buffer[],
 		      int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[],
 		     int flags, int width, int precision, int size);
+
 int print_hexa(va_list types, char map_to[],
 	       char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-/* Function to print non-printable ASCII Characters */
+/* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
 			int flags, int width, int precision, int size);
 
@@ -87,11 +88,11 @@ int get_size(const char *format, int *i);
 int print_reverse(va_list types, char buffer[],
 		  int flags, int width, int precision, int size);
 
-/* Function to print a string in rot13 ENCODING*/
+/* Function to print a string in rot 13 */
 int print_rot13string(va_list types, char buffer[],
 		      int flags, int width, int precision, int size);
 
-/* Fuctions to handle width */
+/* width handler */
 int handle_write_char(char c, char buffer[],
 		      int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
@@ -100,11 +101,12 @@ int write_num(int ind, char bff[], int flags, int width, int precision,
 	      int length, char padd, char extra_c);
 int write_pointer(char buffer[], int ind, int length,
 		  int width, int flags, char padd, char extra_c, int padd_start);
+
 int write_unsgnd(int is_negative, int ind,
 		 char buffer[],
 		 int flags, int width, int precision, int size);
 
-/* UTILS */
+/*UTILS */
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
@@ -112,4 +114,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif
+#endif /* MAIN_H */
